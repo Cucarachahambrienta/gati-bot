c21187b8bb7492db44a185e19c301ede94feea85
g4tito <84881966+g4tito@users.noreply.github.com>
2021-07-17 06:56:26 -0500

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+echo -e '\e[1;32m
+
+Restableciendo...
+
+\e[0m'
+
+rm -r gati-bot.json
+
+sleep 2.0
+
+echo -e '\033[0m\033[1;31m
+
+LISTO!! YA PUEDES VOLVER A ESCANEAR EL CODIGO
+
+\033[0m'
