03d5aef47783fb7bc97487401b0225b25e6e3179
g4tito <84881966+g4tito@users.noreply.github.com>
2021-07-17 14:00:04 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 #!bin/bash
-pkg update -y
-pkg upgrade -y
 
 CYAN="\033[1;36m"
 YELLOW="\033[1;33m"
@@ -9,26 +7,103 @@ GREEN="\033[1;32m"
 RED='\033[1;31m'
 NC='\033[0m'
 
+echo -e " ${YELLOW}Iniciando..."
+sleep 4
+
+clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
+echo -e " ${CYAN}Instalando todas las dependencias ..." | lolcat -a -d 50 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando el comando: pkg update"
+echo -e " ${GREEN}"
+sleep 5
+pkg update -y
+sleep 2
+
+clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando el comando: pkg upgrade"
+echo -e " ${GREEN}"
+sleep 5
+pkg upgrade -y
+sleep 2
+
+clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
 echo -e $YELLOW"Seguro que quieres actualizar a gati-bot? [s/n]"
 read -p "> " opts
 
 if [ $opts = "s" ]; then
 
 clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
 echo -e $YELLOW"Actualizando gati-bot, espere..."
 git clone https://github.com/g4tito/gati-bot > /dev/null 2> /dev/null
 
+clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando el comando: cd gati-bot"
+echo -e " ${GREEN}"
+sleep 5
 cd gati-bot
-cp -r * ..
-cd ..
-rm -rf gati-bot
-npm i
+sleep 2
 
 clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando el comando: cp -r * .."
+echo -e " ${GREEN}"
+sleep 5
+cp -r * ..
+sleep 2
+
+clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando el comando: cd .."
+echo -e " ${GREEN}"
+sleep 5
+cd ..
+sleep 2
+
+clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando el comando: rm -rf gati-bot"
+echo -e " ${GREEN}"
+sleep 5
+rm -rf gati-bot
+sleep 2
+
+clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando el comando: npm i"
+echo -e " ${GREEN}"
+sleep 5
+npm i
+sleep 2
+
+clear
+figlet -c -f slant -t 'gati-bot' | lolcat 
+
 echo -e $GREEN"gati-bot actualizado con exito!!"
 
 elif [ $opts = "n"]; then
     clear
+    figlet -c -f slant -t 'gati-bot' | lolcat 
     echo -e $RED"La actualizacion de gati-bot se canceló"
     exit
 fi
