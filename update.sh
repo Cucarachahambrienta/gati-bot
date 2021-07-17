01a2ab8085c2c3e2240174885d585354592e4895
g4tito <84881966+g4tito@users.noreply.github.com>
2021-07-17 01:05:38 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!bin/bash
+pkg update -y
+pkg upgrade -y
+
+CYAN="\033[1;36m"
+YELLOW="\033[1;33m"
+PURPLE="\033[1;35m"
+GREEN="\033[1;32m"
+RED='\033[1;31m'
+NC='\033[0m'
+
+echo $YELLOW"Seguro que quieres actualizar a gati-bot? [s/n]"
+read -p "> " opts
+
+if [ $opts = "s" ]; then
+
+clear
+echo $YELLOW"Actualizando gati-bot, espere..."
+git clone https://github.com/g4tito/gati-bot > /dev/null 2> /dev/null
+
+cd gati-bot
+cp -r * ..
+cd ..
+rm -rf gati-bot
+npm i
+
+clear
+echo $GREEN"gati-bot actualizado con exito!!"
+
+elif [ $opts = "n"]; then
+    clear
+    echo $RED"La actualizacion de gati-bot se canceló"
+    exit
+fi
