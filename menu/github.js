6f2c23b2323b75d81c95b9a2f698b98329afb4bf
g4tito <84881966+g4tito@users.noreply.github.com>
2021-07-17 20:20:38 -0500

    Create github.js

---
+++ menu/github.js
@@ -0,0 +1,16 @@
+const github = () => {
+	return `
+╭──────────────
+│
+│ *🐱github del creador*
+│ _github.com/g4tito_
+│
+│
+│ *🤖github del bot*
+│ _github.com/g4tito/gati-bot_
+│
+╰──────────────
+`
+}
+
+exports.github = github
